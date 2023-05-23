4653dbd37d03d036711935ff9d118fcaae8fcb57
Jayson Amati <130301788+jaysonamati@users.noreply.github.com>
2023-05-23 10:11:16 +0300

    Revert "Fp 14"

--- android/app/src/main/java/com/fuse/MainActivity.java
+++ android/app/src/main/java/com/fuse/MainActivity.java
@@ -1,7 +1,5 @@
 package com.fuse;
 
-import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -16,11 +14,6 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "fuse";
-  }
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
   }
 
   /**
