93fd86f12b942736bb3cf5b07246acf1c9cb66ac
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-05-31 16:23:42 +0300

    Add string constants file for global string reference

---
+++ src/constants/AppStrings.ts
@@ -0,0 +1,24 @@
+//http headers 
+const CONTENT_TYPE = 'Content-Type';
+const AUTHORIZATION = 'Authorization';
+
+//response codes
+const RESPONSE_OK = 200;
+const RESPONSE_CREATED = 201;
+const SERVER_ERROR = 500; 
+const CLIENT_ERROR = 400;
+
+//routes
+const SCR_KYC = 'KYCScreen';
+const SCR_LOGIN= 'LoginScreen';
+const SCR_REGISTRATION = 'RegistrationScreen';
+const SCR_WALLET = 'WalletScreen';
+const SCR_TABNAVIGATION = 'TabNavigationScreen';
+
+export {
+    SCR_KYC,
+    SCR_REGISTRATION,
+    SCR_WALLET,
+    SCR_LOGIN,
+    SCR_TABNAVIGATION
+}
