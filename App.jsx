f0d6c8919b097b1127abb81753046acc390fc046
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-05-31 16:25:10 +0300

    Remove stack navigator from app entry point

--- App.jsx
+++ App.jsx
@@ -1,47 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SignUpForm, RegisterFormKYC } from './src/screens/SignUpForm';
-import LoginScreen from './src/screens/Login';
 import TabNavigator from './src/components/TabNavigation';
 
-const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='SignUpForm'>
-            <>
-              <Stack.Screen
-                name="Login"
-                component={LoginScreen}
-                options={{
-                  title: 'Login',
-                }}
-              />
-              <Stack.Screen
-                name="SignUpForm"
-                component={SignUpForm}
-                options={{
-                  title: 'Create Account',
-                }}
-              />
-              <Stack.Screen
-                name="KYCForm"
-                component={RegisterFormKYC}
-                options={{
-                  title: 'KYC Details',
-                }}
-              />
-            </>
-              <Stack.Screen
-                name="HomeScreen"
-                component={TabNavigator}
-                options={{
-                  title: 'Fuse',
-                }}
-              />
-        </Stack.Navigator>
+                <TabNavigator/>
     </NavigationContainer>
   );
 }
