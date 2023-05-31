dc0049bbad6f2fb3d9d005efe04482c12086d547
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-05-31 19:23:18 +0300

    Rework tab navigation bar styling

--- App.jsx
+++ App.jsx
@@ -6,7 +6,9 @@ import { WalletScreen } from './src/screens/Wallet';
 import { BuyAssetScreen } from './src/screens/BuyAsset';
 import { PaymentsScreen } from './src/screens/Payments';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { SCR_WALLET } from './src/constants/AppStrings';
+
+import { SCR_BUY_ASSET, SCR_MAKE_PAYMENTS, SCR_WALLET } from './src/constants/AppStrings';
+import Theme from './src/resources/assets/Style';
 
 function App() {
 
@@ -15,22 +17,25 @@ function App() {
     <NavigationContainer independent={true}>
       <Tab.Navigator
         initialRouteName={SCR_WALLET}
-        screenOptions={{
-          tabBarActiveTintColor: '#e91e63',
-          headerShown: false,
-        }}
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+           let iconName;
+           if(route.name == SCR_WALLET){
+            iconName = focused ? 'wallet' : 'wallet-outline';
+           } else if (route.name === SCR_BUY_ASSET) {
+            iconName = focused ? 'transfer' : 'transfer-outline';
+           } else if (route.name === SCR_MAKE_PAYMENTS){
+            iconName = focused ? 'smart-card' : 'smart-card-outline';
+           } 
+          return <MaterialCommunityIcons name={iconName} size={28} color={color}/>;
+          },
+          tabBarActiveTintColor: Theme.fsColors.secondary,
+          tabBarInactiveTintColor: 'gray',
+        })}
       >
-        <Tab.Screen
-          name={SCR_WALLET}
-          component={WalletScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen name="BuyAda" component={BuyAssetScreen} />
-        <Tab.Screen name="Make Payments" component={PaymentsScreen} />
+        <Tab.Screen name={SCR_WALLET} component={WalletScreen}/>
+        <Tab.Screen name="BuyAsset" component={BuyAssetScreen} />
+        <Tab.Screen name="MakePayments" component={PaymentsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- src/constants/AppStrings.ts
+++ src/constants/AppStrings.ts
@@ -13,10 +13,14 @@ const SCR_KYC = 'KYC';
 const SCR_LOGIN= 'Login';
 const SCR_REGISTRATION = 'Registration';
 const SCR_WALLET = 'Wallet';
+const SCR_BUY_ASSET = 'BuyAsset';
+const SCR_MAKE_PAYMENTS = 'MakePayments';
 
 export {
     SCR_KYC,
     SCR_REGISTRATION,
     SCR_WALLET,
     SCR_LOGIN,
+    SCR_BUY_ASSET,
+    SCR_MAKE_PAYMENTS,
 }
