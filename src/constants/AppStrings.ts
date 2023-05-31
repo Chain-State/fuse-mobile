6c363bbd3be10371050d6f08143cb2862540c30f
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-05-31 16:37:05 +0300

    Rename string targets for constants

--- src/constants/AppStrings.ts
+++ src/constants/AppStrings.ts
@@ -9,16 +9,14 @@ const SERVER_ERROR = 500;
 const CLIENT_ERROR = 400;
 
 //routes
-const SCR_KYC = 'KYCScreen';
-const SCR_LOGIN= 'LoginScreen';
-const SCR_REGISTRATION = 'RegistrationScreen';
-const SCR_WALLET = 'WalletScreen';
-const SCR_TABNAVIGATION = 'TabNavigationScreen';
+const SCR_KYC = 'KYC';
+const SCR_LOGIN= 'Login';
+const SCR_REGISTRATION = 'Registration';
+const SCR_WALLET = 'Wallet';
 
 export {
     SCR_KYC,
     SCR_REGISTRATION,
     SCR_WALLET,
     SCR_LOGIN,
-    SCR_TABNAVIGATION
 }
