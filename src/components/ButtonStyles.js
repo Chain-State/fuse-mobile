a071d006b6ccc41321a8e6ca8961f46805ca7da4
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-05-31 16:23:02 +0300

    Rename custom button comp file

---
+++ src/components/ButtonStyles.js
@@ -0,0 +1,26 @@
+const { StyleSheet } = require("react-native");
+
+const styles = StyleSheet.create({
+    appButtonContainer: {
+        fontFamily: 'Montserrat-Regular',
+        borderRadius: 8,
+        margin: 15,
+        height: 50,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        elevation: 5,
+        backgroundColor: '#4CAF50',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    appButtonText: {
+        lineHeight: 21,
+        fontSize: 18,
+        fontWeight: 'bold',
+        letterSpacing: 0.75,
+        color: '#FFFFFF'
+    }
+});
+
+export default styles;
