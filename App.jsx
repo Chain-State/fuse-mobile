62eb0f8a184ddf79176855c4537e94adb2400091
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-06-01 08:56:08 +0300

    Code styling updated

--- App.jsx
+++ App.jsx
@@ -7,32 +7,34 @@ import { BuyAssetScreen } from './src/screens/BuyAsset';
 import { PaymentsScreen } from './src/screens/Payments';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { SCR_BUY_ASSET, SCR_MAKE_PAYMENTS, SCR_SWAP_TOKENS, SCR_WALLET } from './src/constants/AppStrings';
+import {
+  SCR_BUY_ASSET,
+  SCR_MAKE_PAYMENTS,
+  SCR_SWAP_TOKENS,
+  SCR_WALLET,
+} from './src/constants/AppStrings';
 import Theme from './src/resources/assets/Style';
 
 function App() {
-
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer independent={true}>
       <Tab.Navigator
         initialRouteName={SCR_WALLET}
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-           let iconName;
-           if(route.name == SCR_WALLET){
-            iconName = focused ? 'wallet' : 'wallet-outline';
-           } else if (route.name === SCR_BUY_ASSET) {
-            iconName = focused ? 'circle-multiple' : 'circle-multiple-outline';
-           } else if (route.name === SCR_MAKE_PAYMENTS){
-            iconName = focused ? 'credit-card' : 'credit-card-outline';
-           } else if (route.name === SCR_SWAP_TOKENS){
-            iconName = focused ? 'swap-horizontal' : 'swap-horizontal';
-           } 
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name == SCR_WALLET) {
+              iconName = focused ? 'wallet' : 'wallet-outline';
+            } else if (route.name === SCR_BUY_ASSET) {
+              iconName = focused ? 'circle-multiple' : 'circle-multiple-outline';
+            } else if (route.name === SCR_MAKE_PAYMENTS) {
+              iconName = focused ? 'credit-card' : 'credit-card-outline';
+            } else if (route.name === SCR_SWAP_TOKENS) {
+              iconName = focused ? 'swap-horizontal' : 'swap-horizontal';
+            }
 
-
-
-          return <MaterialCommunityIcons name={iconName} size={size} color={color}/>;
+            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: Theme.fsColors.secondary,
           tabBarInactiveTintColor: 'gray',
@@ -41,7 +43,7 @@ function App() {
           tabBarStyle: Theme.fsTabNavigation.tab,
         })}
       >
-        <Tab.Screen name={SCR_WALLET} component={WalletScreen}/>
+        <Tab.Screen name={SCR_WALLET} component={WalletScreen} />
         <Tab.Screen name={SCR_BUY_ASSET} component={BuyAssetScreen} />
         <Tab.Screen name={SCR_MAKE_PAYMENTS} component={PaymentsScreen} />
         <Tab.Screen name={SCR_SWAP_TOKENS} component={PaymentsScreen} />
