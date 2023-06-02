ee95ca1bb411490e568c44903182a7d990c47ea5
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-06-02 20:34:42 +0300

    Add dummy data file for line chart

---
+++ src/data/dummy/LineChartDummyData.js
@@ -0,0 +1,1 @@
+export default lineChartData = [{ value: 37 }, { value: 24 }, { value: 38 }, { value: 30 }, { value: 27 }, { value: 28 }, { value: 18 }, { value: 21 }, { value: 27 }, { value: 28 }, { value: 18 }, { value: 21 }]
