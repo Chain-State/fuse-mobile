190845dadc42c3bb72bb7598191ff10fbc7f88a9
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-06-03 13:07:46 +0300

    Update line chart for ux

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -5,25 +5,42 @@ import Theme from '../resources/assets/Style';
 
 function AssetPriceLineChart({ title, chartData }) {
   return (
-    <View>
-      <Text>{title}</Text>
-      <LineChart
-        data={chartData}
-        curved={true}
-        stepValue={10}
-        hideDataPoints={true}
-        hideYAxisText={true}
-        thickness={2}
-        color={Theme.fsColors.secondary}
-        showVerticalLines={false}
-        spacing={35}
-        initialSpacing={0}
-        xAxisLength={500}
-        hideRules={true}
-        yAxisColor={Theme.fsColors.backgroundColor}
-        xAxisColor={Theme.fsColors.backgroundColor}
-      />
-    </View>
+    <>
+      <View>
+        <Text
+          style={{
+            padding: 5,
+            marginTop: 20,
+            fontFamily: Theme.fsFonts.boldFont.fontFamily,
+            fontSize: 22,
+            marginBottom: 10,
+          }}
+        >
+          ADA: KES 36.34
+        </Text>
+        <LineChart
+          data={chartData}
+          curved={true}
+          stepValue={5}
+          minValue={0}
+          noOfSections={7}
+          hideDataPoints={true}
+          yAxisLabelPrefix="Ksh"
+          thickness={1}
+          color={Theme.fsColors.secondary}
+          showVerticalLines={false}
+          // spacing={35}
+          rulesColor={Theme.fsColors.secondary}
+          initialSpacing={0}
+          yAxisOffset={0}
+          // noOfSectionsBelowXAxis={0}
+          yAxisLabelWidth={50}
+          yAxisIndicesHeight={20}
+          yAxisColor={Theme.fsColors.backgroundColor}
+          xAxisColor={Theme.fsColors.backgroundColor}
+        />
+      </View>
+    </>
   );
 }
 
--- src/data/dummy/LineChartDummyData.js
+++ src/data/dummy/LineChartDummyData.js
@@ -1,1 +1,1 @@
-export default lineChartData = [{ value: 37 }, { value: 24 }, { value: 38 }, { value: 30 }, { value: 27 }, { value: 28 }, { value: 18 }, { value: 21 }, { value: 27 }, { value: 28 }, { value: 18 }, { value: 21 }]
+export default lineChartData = [{ value: 22 }, { value: 24 }, { value: 38 }, { value: 30 }, { value: 27 }, { value: 28 }, { value: 18 }, { value: 21 }, { value: 27 }, { value: 28 }, { value: 18 }, { value: 21 }]
