6b38babf0b9017115ce346528d5c9af8c810c92e
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-06-11 16:36:20 +0300

    Add DEBUG function to spit keys

--- src/screens/Wallet.jsx
+++ src/screens/Wallet.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View, FlatList } from 'react-native';
 import { transactionDummyData, pieDummyData } from '../data/dummy/transactions';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { DonutGraphWithLegend } from '../components/DonutChart';
 import Theme from '../resources/assets/Style';
 
 export const WalletScreen = ({ navigation, route }) => {
+  importData = async () => {
+    try {
+      const keys = await AsyncStorage.getAllKeys();
+      const result = await AsyncStorage.multiGet(keys);
+
+      return result.map((req) => console.log(req));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    importData();
+  }, []);
   return (
     <View flex={1}>
       <DonutGraphWithLegend pieData={pieDummyData} />
