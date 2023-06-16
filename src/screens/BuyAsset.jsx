ead49dd0f6cb94db2c9e543c2a6ad93cc7977c19
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-06-16 22:50:41 +0300

    Refactor token+local ex rate functions and fix exchange rate fetch

--- src/screens/BuyAsset.jsx
+++ src/screens/BuyAsset.jsx
@@ -22,50 +22,57 @@ const BuyAssetScreen = ({ navigation }) => {
   const [amountAda, setAmountAda] = useState('');
   const [amountFiat, setAmountFiat] = useState('');
   const [priceHistory, setPriceHistory] = useState([]);
-  const [exchangeRate, setExchangeRate] = useState(140);
+  const [exchangeRate, setExchangeRate] = useState(null);
   const [adaPrice, setAdaPrice] = useState(0.2564);
   const [isProcessing, setIsProcessing] = useState(false);
 
+  const getTokenExRate = () => {
+    fetch(
+      'https://rest.coinapi.io/v1/exchangerate/ADA/USD/history?period_id=1DAY&time_start=2023-06-11T23:59:00.0000000Z&time_end=2023-06-15T23:59:00.0000000Z&display_name=day',
+      {
+        method: 'GET',
+        headers: {
+          'X-CoinAPI-Key': '7CC0179B-877B-4473-AD94-B0AB33921F18',
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(JSON.stringify(data));
+        const priceData = data.map((item) => {
+          item = item;
+          delete item.time_period_start;
+          delete item.time_period_end;
+          delete item.rate_low;
+          delete item.rate_high;
+          delete item.rate_open;
+          delete item.time_open;
+          delete item.time_close;
+          item['value'] = (item.rate_close * 142).toFixed(2);
+          return item;
+        });
+        console.log(`PriceData == ${JSON.stringify(priceData)}`);
+        setPriceHistory(priceData);
+      })
+      .catch((error) => console.log(`Price fetch failed: ${error}`));
+  };
+
+  const getLocalCurrencyRate = () => {
+    fetch('https://openexchangerates.org/api/latest.json?app_id=46d02af8d01c44118f232980cbad46a8', {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.rates['KES']);
+        setExchangeRate(data.rates['KES']);
+      })
+      .catch((err) => console.log(`Fx rate fetch failed: ${err}`));
+  };
+
   useEffect(() => {
-    //get ADA price history.Default 1 week
-    // fetch(
-    //   'https://rest.coinapi.io/v1/exchangerate/ADA/USD/history?period_id=1DAY&time_start=2023-06-11T23:59:00.0000000Z&time_end=2023-06-15T23:59:00.0000000Z&display_name=day',
-    //   {
-    //     method: 'GET',
-    //     headers: {
-    //       'X-CoinAPI-Key': '7CC0179B-877B-4473-AD94-B0AB33921F18',
-    //     },
-    //   }
-    // )
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log(JSON.stringify(data));
-    //     const priceData = data.map((item) => {
-    //       item = item;
-    //       delete item.time_period_start;
-    //       delete item.time_period_end;
-    //       delete item.rate_low;
-    //       delete item.rate_high;
-    //       delete item.rate_open;
-    //       delete item.time_open;
-    //       delete item.time_close;
-    //       item['value'] = (item.rate_close * 142).toFixed(2);
-    //       return item;
-    //     });
-    //     console.log(`PriceData == ${JSON.stringify(priceData)}`);
-    //     setPriceHistory(priceData);
-    //   })
-    //   .catch((error) => console.log(`Price fetch failed: ${error}`));
-    // fetch('https://openexchangerates.org/api/latest.json?app_id=46d02af8d01c44118f232980cbad46a8', {
-    //   method: 'GET',
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log(data.rates['KES']);
-    //     setExchangeRate(data['KES']);
-    //   })
-    //   .catch((err) => console.log(`Fx rate fetch failed: ${err}`));
-  }, []);
+    // getTokenExRate();
+    getLocalCurrencyRate();
+  }, [exchangeRate]);
 
   return (
     <>
